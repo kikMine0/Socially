52b29edf464ae0c85fafd8b0cb2b0ba301819cc7
albe <kikmine090511@gmail.com>
2026-08-20 14:31:27 +0200

    fix getMessages()

--- src/main/java/kik/socially/ConfigManager.java
+++ src/main/java/kik/socially/ConfigManager.java
@@ -22,6 +22,6 @@ public class ConfigManager {
         return configurazione.getConfigurationSection("socials");
     }
     public List<String> getMessages(String social){
-        return configurazione.getStringList(social);
+        return configurazione.getStringList("socials."+social);
     }
 }
