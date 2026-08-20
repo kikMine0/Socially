c334cff82cdbb201bf541f7a3b4999f106a9df4b
albe <kikmine090511@gmail.com>
2026-08-20 14:14:42 +0200

    holy tuff

--- src/main/java/kik/socially/CommandManager.java
+++ src/main/java/kik/socially/CommandManager.java
@@ -1,4 +1,12 @@
 package kik.socially;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
+import org.bukkit.plugin.Plugin;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CommandManager {
 }
