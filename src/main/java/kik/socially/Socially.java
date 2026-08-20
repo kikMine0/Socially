d32902ea1217bdb2f2cf6fd3712837340d3058f7
albe <kikmine090511@gmail.com>
2026-08-20 12:47:50 +0200

    Initial commit

---
+++ src/main/java/kik/socially/Socially.java
@@ -0,0 +1,16 @@
+package kik.socially;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Socially extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/kik/socially/SociallyLoader.java
@@ -0,0 +1,12 @@
+package kik.socially;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+
+class SociallyLoader implements PluginLoader {
+
+    @Override
+    public void classloader(final PluginClasspathBuilder builder) {
+        // Add dynamically loaded libraries here
+    }
+}
