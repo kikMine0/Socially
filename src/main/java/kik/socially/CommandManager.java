a95af7a176ac18f2fbd5d426c7ffdfa4ce3a80f0
albe <kikmine090511@gmail.com>
2026-08-20 14:19:17 +0200

    base

--- src/main/java/kik/socially/CommandManager.java
+++ src/main/java/kik/socially/CommandManager.java
@@ -9,4 +9,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CommandManager {
+    private final Plugin plugin;
+    private final CommandMap commandMap;
+    private final Map<String, Command> cmdregistrati = new HashMap<>();
+    public CommandManager(Plugin plugin) {
+        this.plugin = plugin;
+        this.commandMap = Bukkit.getCommandMap();
+    }
+    public void registraCMDS(){
+
+    }
 }
